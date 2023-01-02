e8fb4de2b5434366e629fa6bc1144dc695135f58
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-01-03 00:34:37 +0300

    Add files via upload

---
+++ ChallengeProject/ChallengeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeProject
+//
+//  Created by Gökhan Gökoğlan on 3.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
