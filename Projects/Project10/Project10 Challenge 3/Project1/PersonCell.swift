47a4a36f18cf3ac54b33eef8087f4585fd39ff31
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-03-07 10:30:36 +0300

    Add files via upload

---
+++ Projects/Project10/Project10 Challenge 3/Project1/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project1
+//
+//  Created by Gökhan Gökoğlan on 7.03.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
